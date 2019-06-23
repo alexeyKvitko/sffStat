5934721dab64bd677740223cf48c9dc264942a46
alexeyKvitko <kpk_08@mail.ru>
2019-06-23 20:58:32 +0300

    add splash activity

--- app/src/main/java/ru/sff/statistic/activity/MainActivity.java
+++ app/src/main/java/ru/sff/statistic/activity/MainActivity.java
@@ -2,11 +2,12 @@ package ru.sff.statistic.activity;
 
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
+import ru.sff.statistic.AppConstants;
 import ru.sff.statistic.R;
 import ru.sff.statistic.fragment.MenuFragment;
 
@@ -16,20 +17,31 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnMe
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
-        addReplaceFragment( MenuFragment.newInstance() );
+        initialize();
+//        addReplaceFragment( MenuFragment.newInstance() );
+    }
+
+
+    private void initialize(){
+        ( (TextView) findViewById( R.id.menuSelectTitleId )).setTypeface(AppConstants.ROTONDA_BOLD );
+        ( (TextView) findViewById( R.id.queryAllDrawsId )).setTypeface(AppConstants.ROBOTO_CONDENCED );
+        ( (TextView) findViewById( R.id.queryByDrawsId )).setTypeface(AppConstants.ROBOTO_CONDENCED );
+        ( (TextView) findViewById( R.id.queryByMagnetId )).setTypeface(AppConstants.ROBOTO_CONDENCED );
+        ( (TextView) findViewById( R.id.queryByDateId)).setTypeface(AppConstants.ROBOTO_CONDENCED );
+        ( (TextView) findViewById( R.id.lotoTypeId )).setTypeface(AppConstants.ROBOTO_CONDENCED );
     }
 
 
     protected void addReplaceFragment( Fragment newFragment ) {
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.setCustomAnimations( R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out );
-        if ( getSupportFragmentManager().getFragments().size() == 0 ) {
-            fragmentTransaction.add( R.id.menuFragmentContainerId, newFragment );
-        } else {
-            fragmentTransaction.replace( R.id.menuFragmentContainerId, newFragment );
-            fragmentTransaction.addToBackStack( null );
-        }
-        fragmentTransaction.commit();
+//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//        fragmentTransaction.setCustomAnimations( R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out );
+//        if ( getSupportFragmentManager().getFragments().size() == 0 ) {
+//            fragmentTransaction.add( R.id.menuFragmentContainerId, newFragment );
+//        } else {
+//            fragmentTransaction.replace( R.id.menuFragmentContainerId, newFragment );
+//            fragmentTransaction.addToBackStack( null );
+//        }
+//        fragmentTransaction.commit();
     }
 
     @Override
